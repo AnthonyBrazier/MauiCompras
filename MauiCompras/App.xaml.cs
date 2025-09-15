f82e8c4aa9ae817f324a25e4ba130e6513f21ff7
AnthonyBrazier <anthonybleite@gmail.com>
2025-09-14 22:47:09 -0300

    Add do R$ na lista e Refreshing para regarregar a pagina

--- MauiCompras/App.xaml.cs
+++ MauiCompras/App.xaml.cs
@@ -29,10 +29,11 @@ namespace MauiCompras
         }
 
 
-
         public App()
         {
             InitializeComponent();
+
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("pt-BR");
 
             //MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
--- MauiCompras/Views/ListaProduto.xaml.cs
+++ MauiCompras/Views/ListaProduto.xaml.cs
@@ -51,6 +51,8 @@ public partial class ListaProduto : ContentPage
 
             string q = e.NewTextValue;
 
+            lst_produtos.IsRefreshing = true;
+
             lista.Clear();
 
             List<Produto> tmp = await App.Db.Search(q);
@@ -61,6 +63,10 @@ public partial class ListaProduto : ContentPage
         catch (Exception ex)
         {
             await DisplayAlert("Ops", ex.Message, "OK");
+        }
+        finally
+        {
+            lst_produtos.IsRefreshing = false;
         }
     }
 
@@ -137,4 +143,22 @@ public partial class ListaProduto : ContentPage
         }
     }
 
+    private async void lst_produtos_Refreshing(object sender, EventArgs e)
+    {
+        try
+        {
+            lista.Clear();
+            List<Produto> tmp = await App.Db.GetAll();
+
+            tmp.ForEach(i => lista.Add(i));
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops!", ex.Message, "OK");
+        } finally
+        { 
+            lst_produtos.IsRefreshing = false; 
+        }
+
+    }
 }
